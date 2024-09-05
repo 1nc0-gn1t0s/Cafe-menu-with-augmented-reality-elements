735eb3d1ebf7b14d9d92b19932281a6ccead1373
1nc0-gn1t0s <151000902+1nc0-gn1t0s@users.noreply.github.com>
2024-09-05 12:25:58 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,103 +1,99 @@
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, Flask, request, redirect, url_for, flash
 from flask_login import LoginManager, login_user, logout_user, login_required
 from werkzeug.security import check_password_hash
 
-from models import app, User, create_user, get_user_by_username
+
+from models import app, get_user_by_email, create_user, User
 
 
-login_manager = LoginManager()
-login_manager.init_app(app)
+"""login_manager = LoginManager()
+login_manager.init_app(app)"""
 
 
-@login_manager.user_loader
-def load_user(user_id):
+@app.route('/')
+def index():
     """
-    Функция для поиска пользователя по id
-    :param user_id: id
-    :return: User
-    """
-    return User.query.get(int(user_id))
-
-
-@app.route('/user/<username>')
-@login_required
-def user(username):
-    """
-    Возвращает страницу пользователя
-    :param username: username
+    Возвращает домашнюю страницу
     :return: html
     """
-    user_0 = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user_0)
+    return render_template('index.html')
 
 
-@app.route('/registration', methods=['GET', 'POST'])
-def registration():
+@app.route('/contacts')
+def contacts():
     """
-    Функция для регистрации пользователя; открывает html соответствующей страницы
+    Возвращает страницу с обратной связью
+    :return: html
+    """
+    return render_template('contacts.html')
+
+
+@app.route('/restaurants')
+def restaurants():
+    """
+    Возвращает страницу с меню ресторанов
+    :return: html
+    """
+    return render_template('restaurants.html')
+
+
+@app.route('/log_in', methods=['GET', 'POST'])
+def log_in():
+    """
+    Возвращает страницу входа в аккаунт
     :return: html
     """
     if request.method == 'POST':
         info = list(dict(request.form).values())
-        nickname, username, password, repeat_password = info[0], info[1], info[2], info[3]
-        if not (nickname and username and password and repeat_password):
-            flash('All fields must be filled in.')
-            return redirect(url_for('registration'))
+        print(info)
+        email, password = info[0], info[1]
+        if not email or not password:
+            flash('Все поля должны быть заполнены.')
+            return redirect(url_for('log_in'))
+
+        user = get_user_by_email(email)
+
+        if not user:
+            flash('Пользователя с такой почтой не существует.')
+            return redirect(url_for('log_in'))
+
+        if not check_password_hash(user.password_hash, password):
+            flash('Пароль введен неправильно.', 'error')
+            return redirect(url_for('log_in'))
+
+        login_user(user)
+        return redirect(url_for('user', email=email))
+
+    return render_template('log_in.html')
+
+
+@app.route('/sigh_up', methods=['GET', 'POST'])
+def sigh_up():
+    """
+    Возвращает страницу регистрации
+    :return: html
+    """
+    if request.method == 'POST':
+        info = list(dict(request.form).values())
+        name, email, password, repeat_password = info[0], info[1], info[2], info[3]
+        if not (name and email and password and repeat_password):
+            flash('Все поля должны быть заполнены.')
+            return redirect(url_for('sigh_up'))
 
         if password != repeat_password:
-            flash('You repeated the password incorrectly.')
-            return redirect(url_for('registration'))
+            flash('Пароли не совпадают.')
+            return redirect(url_for('sigh_up'))
 
-        if get_user_by_username(username):
-            flash('The name is occupied.')
-            return redirect(url_for('registration'))
+        if get_user_by_email(email):
+            flash('Пользователь с такой почтой уже существует.')
+            return redirect(url_for('sigh_up'))
 
-        create_user(nickname, username, password)
+        create_user(1, name, email, password)
 
-        flash("You're successfully sighed up. Please log in.")
-        return redirect(url_for('login'))
+        flash("Вы успешно зарегистрированы! Пожалуйста, войдите в аккаунт.")
+        return redirect(url_for('log_in'))
 
-    return render_template('registration.html')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    """
-    Функция для входа пользователей в аккаунты; открывает html соответствующей страницы
-    :return: html
-    """
-    if request.method == 'POST':
-        info = list(dict(request.form).values())
-        username, password = info[0], info[1]
-        if not username or not password:
-            flash('All fields must be filled in.')
-            return redirect(url_for('login'))
-
-        user_1 = get_user_by_username(username)
-
-        if not user_1:
-            flash('User is not found.')
-            return redirect(url_for('login'))
-
-        if not check_password_hash(user_1.password_hash, password):
-            flash('Wrong password.', 'error')
-            return redirect(url_for('login'))
-
-        login_user(user_1)
-        return redirect(url_for('user', username=username))
-
-    return render_template('login.html')
-
-
-@app.route('/logout')
-@login_required
-def logout():
-    """
-    Функция для выхода из аккаунта пользователя
-    :return: home html
-    """
-    logout_user()
-    return redirect('/')
+    return render_template('sigh_up.html')
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 
 app = Flask(__name__)
-app.secret_key = 'just a really secret sentence'
+app.secret_key = 'there will be something here'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
 
@@ -15,36 +15,102 @@ class User(db.Model, UserMixin):
     Класс пользователя
     """
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(300), nullable=False)
-    nickname = db.Column(db.Text, nullable=False)
+    role = db.Column(db.Integer, primary_key=False, nullable=False)
+    name = db.Column(db.String(300), nullable=False)
+    email = db.Column(db.String(300), nullable=False)
     password_hash = db.Column(db.String(300), nullable=False)
 
-    def __init__(self, nickname, username, password_hash):
-        self.nickname = nickname
-        self.username = username
+    def __init__(self, role, name, email, password_hash):
+        self.role = role
+        self.name = name
+        self.email = email
         self.password_hash = password_hash
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<User {}>'.format(self.name)
 
 
-def create_user(nickname, username, password):
+class RestaurantChain(db.Model):
+    """
+    Класс сетей ресторанов
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300), nullable=False)
+    admin = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, title, admin):
+        self.title = title
+        self.admin = admin
+
+
+class Restaurant(db.Model):
+    """
+    Класс ресторанов
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300), nullable=False)
+    manager = db.Column(db.Integer, nullable=False)
+    address = db.Column(db.String(300), nullable=False)
+    contacts = db.Column(db.String(300), nullable=False)
+
+    def __init__(self, title, manager, address, contacts):
+        self.title = title
+        self.manager = manager
+        self.address = address
+        self.contacts = contacts
+
+    def __repr__(self):
+        return '<Restaurant {}>'.format(self.title)
+
+
+class Dish(db.Model):
+    """
+    Класс блюд
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300), nullable=False)
+    restaurant_chain = db.Column(db.Integer, nullable=False)
+    photo_path = db.Column(db.String(300), nullable=False)
+    ar_path = db.Column(db.String(300), nullable=False)
+
+    def __init__(self, title, restaurant_chain, photo_path,  ar_path):
+        self.title = title
+        self.restaurant_chain = restaurant_chain
+        self.photo_path = photo_path
+        self.ar_path = ar_path
+
+    def __repr__(self):
+        return '<Dish {}>'.format(self.title)
+
+
+def create_user(role, name, email, password):
     """
     Функция для добавления пользователя в базу данных
-    :param nickname: nickname
-    :param username: username
+    :param role: role
+    :param name: name
+    :param email: email
     :param password: password
     :return: None
     """
-    new_user = User(nickname=nickname, username=username, password_hash=generate_password_hash(password))
+    new_user = User(role=role, name=name, email=email, password_hash=generate_password_hash(password))
     db.session.add(new_user)
     db.session.commit()
 
 
-def get_user_by_username(username):
+def get_user_by_email(email):
     """
     Функция для получения пользователя по username
-    :param username: username
+    :param email: email
     :return: None
     """
-    return User.query.filter_by(username=username).first()
+    return User.query.filter_by(email=email).first()
+
+
+def check_password(user, password):
+    """
+    Функция для проверки пароля
+    :param user: user
+    :param password: password
+    :return: Bool
+    """
+    return check_password_hash(user.password_hash, password)
